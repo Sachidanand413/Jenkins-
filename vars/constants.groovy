b7c335e263c98d6f357ffce99cf24de4fbcfda48
Sachidanand413 <67230695+Sachidanand413@users.noreply.github.com>
2022-11-08 17:13:40 +0100

    Update constants.groovy

--- vars/constants.groovy
+++ vars/constants.groovy
@@ -4,7 +4,7 @@ import groovy.transform.Field
 def testFilesList = 'test-package.xml'
 
 def testdataFilesList = 'TestData.xml'
-
+@Field
 def gitId = '9ddfsfc4-fdfdf-sdsd-bd18-fgdgdgdf'
 
 return this;
