d230cfef820f7f80747bfeb8000ad69fa41d1ff1
Sachidanand413 <67230695+Sachidanand413@users.noreply.github.com>
2022-11-14 09:11:03 +0100

    Update config.groovy

--- src/config.groovy
+++ src/config.groovy
@@ -1,15 +1,18 @@
 package k8s.deploy
+import groovy.transform.Field
 
-def configParams = [          
-         "ENVIRONMENT" : "TEST",
-         "maven_file_id" : "21e8f8bc",
-         "maven_target_location" : "/home/jenkins/workspace/settings.xml",
-         "docker_login" : "docker login --username 00000000-0000-0000-0000-000000000000 --password ",
-         "sonar_scanner" : "sonar-scanner_3.2.0",
-         "dependency_check" : "dependency-check_6.5.1",
-         "api_modulebuild" : "api_v1/modulebuild",
-         "api_deployment" : "api_v1/deployment",
-         "api_deployment_request" : "api_v1/deploymentRequest",
-         "go_tool" : "Go_1.19.2"
-]
-
+def config_Params = {
+         configParams = [          
+                  "ENVIRONMENT" : "TEST",
+                  "maven_file_id" : "21e8f8bc",
+                  "maven_target_location" : "/home/jenkins/workspace/settings.xml",
+                  "docker_login" : "docker login --username 00000000-0000-0000-0000-000000000000 --password ",
+                  "sonar_scanner" : "sonar-scanner_3.2.0",
+                  "dependency_check" : "dependency-check_6.5.1",
+                  "api_modulebuild" : "api_v1/modulebuild",
+                  "api_deployment" : "api_v1/deployment",
+                  "api_deployment_request" : "api_v1/deploymentRequest",
+                  "go_tool" : "Go_1.19.2"
+         ]
+         return configParams
+}
