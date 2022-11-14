33dfd3b32d25dfc5a891356d5783b073c13613d9
Sachidanand413 <67230695+Sachidanand413@users.noreply.github.com>
2022-11-14 10:06:09 +0100

    Update first.groovy

--- src/first.groovy
+++ src/first.groovy
@@ -1,9 +1,9 @@
 def test1(){
     //add code for this method
-  echo "From first groovy and test-1 method"
+    echo "From first groovy and test-1 method"
 }
 def test2(){
     //add code for this method
-  echo "From first groovy and test-2 method"
+    echo "From first groovy and test-2 method"
 }
 return this
