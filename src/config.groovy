1bd572e53807f4b007b1a403089ecd6100eef79a
Sachidanand413 <67230695+Sachidanand413@users.noreply.github.com>
2022-11-14 09:26:50 +0100

    Update config.groovy

--- src/config.groovy
+++ src/config.groovy
@@ -11,7 +11,7 @@ def config_Params = {
                   "dependency_check" : "dependency-check_6.5.1",
                   "api_modulebuild" : "api_v1/modulebuild",
                   "api_deployment" : "api_v1/deployment",
-                  "api_deployment_request" : "api_v1/deploymentRequest",this
+                  "api_deployment_request" : "api_v1/deploymentRequest",
                   "go_tool" : "Go_1.19.2"
          ]
          return this
