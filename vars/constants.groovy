35bc6c639c39978cb6ef9a5453113dc3fe639ee9
Sachidanand413 <67230695+Sachidanand413@users.noreply.github.com>
2022-11-14 13:32:02 +0100

    Update constants.groovy

--- vars/constants.groovy
+++ vars/constants.groovy
@@ -6,5 +6,14 @@ def testFilesList = 'test-package.xml'
 def testdataFilesList = 'TestData.xml'
 @Field
 def gitId = '9ddfsfc4-fdfdf-sdsd-bd18-fgdgdgdf'
-
+@Field
+def Host = "127.10.1.0"
+@Field
+def AUTH_TOKEN ="Its token-123"
+@Field
+def moduleBuildId ="404"
+@Field
+def environment = "Eugene-Dev"
+@Field
+def version_api_modulebuild = "src"
 return this;
