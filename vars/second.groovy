cdae9ef740e048c8f6279806257ec65a36b7fdd3
Sachidanand413 <67230695+Sachidanand413@users.noreply.github.com>
2022-11-14 12:42:03 +0100

    Update second.groovy

--- vars/second.groovy
+++ vars/second.groovy
@@ -1,22 +1,22 @@
 import groovy.transform.Field
 @Field private First = null
-def version_api_modulebuild=           "src"
-def version_api_api_deployment=        "src"
-def version_api_api_deployment_request="src"
+stage("test"){
+    def version_api_modulebuild=           "src"
+    def version_api_api_deployment=        "src"
+    def version_api_api_deployment_request="src"
 
-def init(first) {
-    First = first
-}
-def test1(){
-    //add code for this method
-    First.test2()
-    echo "This is from second groovy and test-1 method"
-}
-def updateEugeneModuleBuild(env,moduleBuildId,body,version_api_modulebuild){
-    stage("test"){
-    echo "Here iam $env---$moduleBuildId---$body----$version_api_modulebuild"
-    First.api("$HOST","$AUTH_TOKEN","$moduleBuildId","$body")
-    echo "This is from second groovy and test-2 method"
+    def init(first) {
+        First = first
     }
-return this
+    def test1(){
+        //add code for this method
+        First.test2()
+        echo "This is from second groovy and test-1 method"
+    }
+    def updateEugeneModuleBuild(env,moduleBuildId,body,version_api_modulebuild){
+        echo "Here iam $env---$moduleBuildId---$body----$version_api_modulebuild"
+        First.api("$HOST","$AUTH_TOKEN","$moduleBuildId","$body")
+        echo "This is from second groovy and test-2 method"
+    }
+    return this
 }
