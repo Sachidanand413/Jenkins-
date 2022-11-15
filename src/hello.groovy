30846857968c13d6de472ef2ef72d6dd076f75dd
Sachidanand413 <67230695+Sachidanand413@users.noreply.github.com>
2022-11-15 14:02:45 +0100

    Create hello.groovy

---
+++ src/hello.groovy
@@ -0,0 +1,10 @@
+def api(HOST,AUTH_TOKEN,modulebuildId,body){
+    //add code for this method
+    echo "From first groovy and test-1 method"
+    echo "$HOST----$AUTH_TOKEN-----$modulebuildId-----$body"
+}
+def test2(){
+    //add code for this method
+    echo "From first groovy and test-2 method"
+}
+return this
