57d4725e352aedcca5197470591a1534487e3487
Sachidanand413 <67230695+Sachidanand413@users.noreply.github.com>
2022-11-15 14:10:48 +0100

    Update second.groovy

--- vars/second.groovy
+++ vars/second.groovy
@@ -1,22 +1,22 @@
 import groovy.transform.Field
-@Field private First = null
+@Field private apiModuleBuild = null
 version_api_modulebuild= "src"
 def version_api_api_deployment=        "src"
 def version_api_api_deployment_request="src"
-def init(first) {
-    First = first
+def init(hello) {
+    apiModuleBuild = hello
 }
 
 def test1(){
         //add code for this method
-        First.test2()
+        apiModuleBuild.test2()
         echo "This is from second groovy and test-1 method"
         }
 def updateEugeneModuleBuild(env,moduleBuildId,body,HOST,AUTH_TOKEN){
     stage("test"){
         echo "Here iam $env---$moduleBuildId---$body----$version_api_modulebuild"
         //def var = load ("./constants.groovy")
-        First.api("$HOST","$AUTH_TOKEN","$moduleBuildId","$body")
+        apiModuleBuild.api("$HOST","$AUTH_TOKEN","$moduleBuildId","$body")
         echo "This is from second groovy and test-2 method"
         } 
     }
