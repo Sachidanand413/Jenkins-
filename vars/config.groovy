c53fc6b2ba942e55446a044de4b0cce8b400a5a9
Sachidanand413 <67230695+Sachidanand413@users.noreply.github.com>
2022-11-22 19:18:19 +0100

    Update config.groovy

--- vars/config.groovy
+++ vars/config.groovy
@@ -1,4 +1,5 @@
 package k8s.deploy
+import groovy.transform.Field
 
 def config_params() {
     configParams = [          
@@ -8,10 +9,9 @@ def config_params() {
          "docker_login" : "docker login --username 00000000-0000-0000-0000-000000000000 --password ",
          "sonar_scanner" : "sonar-scanner_3.2.0",
          "dependency_check" : "dependency-check_6.5.1",
-         "api_modulebuild" : "api_v1/modulebuild",
-         "api_deployment" : "api_v1/deployment",
-         "api_deployment_request" : "api_v1/deploymentRequest",
          "go_tool" : "Go_1.19.2"
     ]
     return configParams
 }
+
+return this
