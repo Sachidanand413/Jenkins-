3daa7460f49c32c093f9ee507addd1d20595ce20
Sachidanand413 <67230695+Sachidanand413@users.noreply.github.com>
2022-11-22 16:15:24 +0100

    Update first.groovy

--- src/first.groovy
+++ src/first.groovy
@@ -1,9 +1,9 @@
-def api(HOST,AUTH_TOKEN,modulebuildId,body){
+def firstApi(HOST,AUTH_TOKEN,modulebuildId,body){
     //add code for this method
     echo "From first groovy and test-1 method"
     echo "$HOST----$AUTH_TOKEN-----$modulebuildId-----$body"
 }
-def test2(){
+def firsTest2(){
     //add code for this method
     echo "From first groovy and test-2 method"
 }
