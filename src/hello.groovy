f3984fb52ccbb383e5bec68e4c5da13e5b15ec69
Sachidanand413 <67230695+Sachidanand413@users.noreply.github.com>
2022-11-22 16:14:39 +0100

    Update hello.groovy

--- src/hello.groovy
+++ src/hello.groovy
@@ -1,10 +1,10 @@
-def api(HOST,AUTH_TOKEN,modulebuildId,body){
+def helloApi(HOST,AUTH_TOKEN,modulebuildId,body){
     //add code for this method
-    echo "From first groovy and test-1 method"
+    echo "From hello groovy and test-1 method"
     echo "$HOST----$AUTH_TOKEN-----$modulebuildId-----$body"
 }
-def test2(){
+def helloTest(){
     //add code for this method
-    echo "From first groovy and test-2 method"
+    echo "From hello groovy and test-2 method"
 }
 return this
