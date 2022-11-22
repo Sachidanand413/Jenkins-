80b7c4da421d2b9c45c1c6ef2f85c913791aaab5
Sachidanand413 <67230695+Sachidanand413@users.noreply.github.com>
2022-11-22 16:19:21 +0100

    Update second.groovy

--- vars/second.groovy
+++ vars/second.groovy
@@ -1,26 +1,32 @@
 package k8s.deploy
 import groovy.transform.Field
+
+@Field private apiModuleBuild = null
 @Field private apiModuleBuild = null
 
-initiate = "hello"
-version_api_modulebuild= "src"
-def version_api_api_deployment=        "src"
-def version_api_api_deployment_request="src"
-def init($initiate) {
-    apiModuleBuild = $initiate
+
+def init(){
+    def current_path =sh(script: "pwd", returnStdout: true).trim()
+    
+    def hello_version = "hello"
+    def first_version = "first"    
+    
+    helloModule = load ("${current_path}/API/PublisherAPI/${hello_version}.groovy")
+    firstModule = load ("${current_path}/API/PublisherAPI/${first_version}.groovy")
+    
 }
 
 def test1(){
         //add code for this method
-        apiModuleBuild.test2()
+        firstModule.firstTest2()
         echo "This is from second groovy and test-1 method"
         }
-def updateEugeneModuleBuild(env,moduleBuildId,body,HOST,AUTH_TOKEN){
+def updateHelloModuleBuild(env,moduleBuildId,body,HOST,AUTH_TOKEN){
     //stage("test"){
     echo "Here iam $env---$moduleBuildId---$body----$version_api_modulebuild"
         //def var = load ("./constants.groovy")
         
-    apiModuleBuild.api("$HOST","$AUTH_TOKEN","$moduleBuildId","$body")
+    helloModule.helloApi("$HOST","$AUTH_TOKEN","$moduleBuildId","$body")
     echo "This is from second groovy and test-2 method"
       //  } 
     }
