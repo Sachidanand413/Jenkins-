e6b87d6d319f8cad3557af5e6ea4e4c4b6693268
Sachidanand413 <67230695+Sachidanand413@users.noreply.github.com>
2022-11-23 09:41:47 +0100

    Update hello.groovy

--- src/hello.groovy
+++ src/hello.groovy
@@ -5,6 +5,7 @@ def helloApi(HOST,AUTH_TOKEN,modulebuildId,body){
 }
 def helloTest(){
     //add code for this method
+    echo "Checking the variables/environ ${environment} with build number ${env.BUILD_NUMBER}, my name ${env.NAME}"
     echo "From hello groovy and ${ENVIRONMENT}-2 method with build number ${env.BUILD_NUMBER}"
     echo "${ENVIRONMENT}----${docker_login}-----${sonar_scanner}"
 }
