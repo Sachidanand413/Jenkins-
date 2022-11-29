16ecd7a899b661b7b5d7be9d0ee0ab34437e7a48
Sachidanand413 <67230695+Sachidanand413@users.noreply.github.com>
2022-11-29 19:45:47 +0100

    Update hello.groovy

--- src/hello.groovy
+++ src/hello.groovy
@@ -3,7 +3,7 @@ def helloApi(docker){
     //add code for this method
     echo "From hello groovy and ${docker_login} method"
     echo "----${Host}---${AUTH_TOKEN}-----${modulebuildId}-----${body}---${myName}"
-    value1 = $docker
+    value1 = ${docker}
     if (value1 == 'hello') {
         echo 'Hello from main branch'
         }
